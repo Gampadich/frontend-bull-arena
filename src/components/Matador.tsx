b81f4e6258cc6f81d4b27c29742dc2f5cd895619
Roman Diachenko <165205043+Gampadich@users.noreply.github.com>
2025-11-24 16:51:12 +0200

    first try to move matador to new position

--- src/components/Matador.tsx
+++ src/components/Matador.tsx
@@ -8,16 +8,17 @@ export const Matador = (props : MatadorProps) => {
     const bullRun = (event: BullRunEvent) => {
       const bullPos = event.detail.position;
       let oldPos = matadorPosition
-      while (matadorPosition === bullPos){ 
-        setMatadorPosition(Math.random() * 8)
-        console.log(`Motador is moving from ${oldPos} to ${matadorPosition}`)
+      if(bullPos === oldPos){
+        let random = Math.floor(Math.random() * 8)
+        setMatadorPosition(random)
+        console.log(`Matador is moving from ${oldPos} to ${random}`)
       }
     };
     document.addEventListener('bullRun', bullRun as EventListener)
     return () => {
         document.removeEventListener('bullRun', bullRun as EventListener)
     }
-  });
+  }, [matadorPosition, setMatadorPosition]);
   return (
     <>
       <div>i am motador</div>
