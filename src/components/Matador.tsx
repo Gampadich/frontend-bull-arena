7a954b4c1e809260769304b753a9a7ea64a5d6ad
Roman Diachenko <165205043+Gampadich@users.noreply.github.com>
2025-11-24 20:47:10 +0200

    base logic beta

--- src/components/Matador.tsx
+++ src/components/Matador.tsx
@@ -1,42 +1,38 @@
 import { useState, useEffect } from "react";
 import { BullRunEvent } from "../types/types";
 import { MatadorProps } from "../types/matadorProps";
-import ReactDOM from "react-dom";
 
-export const Matador = (props : MatadorProps) => {
-  const [render, isRender] = useState(false)
-  const {matadorPosition, setMatarodPosition} = props
-  let { applause } = props
-  let oldApplause
-  if(applause === 3){
-    if(applause !== oldApplause){
-      oldApplause = applause
-      isRender(true)
-    } else if (applause === oldApplause){
-      isRender(false)
+export const Matador = (props: MatadorProps) => {
+  const [render, isRender] = useState(false);
+  const { matadorPosition, setMatarodPosition } = props;
+  let { applause } = props;
+  let oldApplause: number;
+  useEffect(() => {
+    if (applause === 3) {
+      if (applause !== oldApplause) {
+        oldApplause = applause;
+        isRender(true);
+      } else if (applause === oldApplause) {
+        isRender(false);
+      }
     }
-  }
+  }, [applause]);
   useEffect(() => {
     const bullRun = (event: BullRunEvent) => {
       const bullPos = event.detail.position;
-      let oldPos = matadorPosition
-      if(bullPos === oldPos){
-        let random = Math.floor(Math.random() * 8)
-        setMatarodPosition(random)
-        console.log(`Matador is moving from ${oldPos} to ${random}`)
+      let oldPos = matadorPosition;
+      if (bullPos === oldPos) {
+        let random = Math.floor(Math.random() * 8);
+        setMatarodPosition(random);
+        console.log(`Matador is moving from ${oldPos} to ${random}`);
       }
     };
-    document.addEventListener('bullRun', bullRun as EventListener)
+    document.addEventListener("bullRun", bullRun as EventListener);
     return () => {
-        document.removeEventListener('bullRun', bullRun as EventListener)
-    }
+      document.removeEventListener("bullRun", bullRun as EventListener);
+    };
   }, [matadorPosition]);
   return (
-    <>
-      {render 
-        ?<div>I am happy motador</div>
-        :<div>i am motador</div>
-      }
-    </>
+    <>{render ? <div>I am happy motador</div> : <div>i am motador</div>}</>
   );
 };
