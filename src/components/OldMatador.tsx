b8a0043036fa1f3db6b08b6b3814783b8812b7ef
Roman Diachenko <165205043+Gampadich@users.noreply.github.com>
2025-11-26 18:43:12 +0200

    project is working final commit

--- src/components/OldMatador.tsx
+++ src/components/OldMatador.tsx
@@ -1,49 +1,88 @@
-import React, { Component, createRef } from "react";
+import React from "react";
 import { BullRunEvent } from "../types/types";
-import { MatadorProps } from "../types/matadorProps"; 
+import { MatadorProps } from "../types/matadorProps";
 import FirstSound from "./Sounds/firstSound.mp3";
 import SecondSound from "./Sounds/SecondSound.mp3";
 import ThirdSound from "./Sounds/ThirdSound.mp3";
 
-export class OldMatador extends Component<MatadorProps> {
+export class OldMatador extends React.PureComponent<MatadorProps> {
     state = {
         render: false,
+        userInteracted: false,
     };
-    oldApplause: number; 
+    oldApplause: number;
+    
+    audioRefs: { [key: number]: HTMLAudioElement | null } = {
+        1: null,
+        2: null,
+        3: null,
+    };
+
     constructor(props: MatadorProps) {
         super(props);
-        this.oldApplause = props.applause; 
+        this.oldApplause = props.applause;
     }
-    playApplauseSound = (applauseValue: number) => {
-        let soundFile;
-        switch (applauseValue) {
-            case 1:
-                soundFile = FirstSound;
-                break;
-            case 2:
-                soundFile = SecondSound;
-                break;
-            case 3:
-                soundFile = ThirdSound;
-                break;
-            default:
-                return;
+
+    componentDidMount() {
+        document.addEventListener("bullRun", this.bullRunHandler as EventListener);
+        
+        window.addEventListener('click', this.handleInitialInteraction, { once: true });
+        window.addEventListener('touchstart', this.handleInitialInteraction, { once: true });
+    }
+    
+    componentWillUnmount() {
+        document.removeEventListener("bullRun", this.bullRunHandler as EventListener);
+        window.removeEventListener('click', this.handleInitialInteraction);
+        window.removeEventListener('touchstart', this.handleInitialInteraction);
+    }
+
+    handleInitialInteraction = () => {
+        if (!this.state.userInteracted) {
+            this.setState({ userInteracted: true });
+            
+            this.audioRefs[1] = new Audio(FirstSound);
+            this.audioRefs[2] = new Audio(SecondSound);
+            this.audioRefs[3] = new Audio(ThirdSound);
         }
-        const audio = new Audio(soundFile);
-        audio.play().catch(e => console.error("Sound playback error:", e));
-    }
-    componentDidUpdate(prevProps: MatadorProps) {
+    };
+
+    playApplauseSound = (applauseValue: number) => {
+        if (!this.state.userInteracted) {
+            return;
+        }
+
+        const audio = this.audioRefs[applauseValue];
+
+        if (audio) {
+            audio.pause();
+            audio.currentTime = 0;
+            audio.play().catch(e => console.error("Sound playback error:", e));
+        }
+    };
+
+    componentDidUpdate(prevProps: MatadorProps, prevState: { render: boolean, userInteracted: boolean }) {
         const { applause } = this.props;
+        const { render } = this.state;
+        let newRenderState = render;
+
         if (applause !== prevProps.applause) {
             this.playApplauseSound(applause);
+
+            if (applause === 3) {
+                newRenderState = true;
+                setTimeout(() => {
+                    this.setState({ render: false });
+                }, 100);
+            } else {
+                newRenderState = false;
+            }
         }
-        if (applause === 3 && applause !== prevProps.applause) {
-            this.setState({ render: true });
-        } else if (applause === 3 && applause === prevProps.applause) {
-        } else if (applause !== 3) {
-            this.setState({ render: false });
+        
+        if (newRenderState !== render && applause !== 3) {
+            this.setState({ render: newRenderState });
         }
     }
+    
     bullRunHandler = (event: BullRunEvent) => {
         const { matadorPosition, setMatarodPosition } = this.props;
         const bullPos = event.detail.position;
@@ -54,12 +93,7 @@ export class OldMatador extends Component<MatadorProps> {
             console.log(`Matador is moving from ${oldPos} to ${random}`);
         }
     };
-    componentDidMount() {
-        document.addEventListener("bullRun", this.bullRunHandler as EventListener);
-    }
-    componentWillUnmount() {
-        document.removeEventListener("bullRun", this.bullRunHandler as EventListener);
-    }
+    
     render() {
         const { render } = this.state;
         return (
