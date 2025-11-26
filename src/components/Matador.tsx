8cca09b619035374315232268a1acaa7f98032bb
Roman Diachenko <165205043+Gampadich@users.noreply.github.com>
2025-11-26 07:11:52 +0200

    add sound logic and it work

--- src/components/Matador.tsx
+++ src/components/Matador.tsx
@@ -1,39 +1,47 @@
 import { useState, useEffect, useRef } from "react";
-import React from 'react'
+import React from "react";
 import { BullRunEvent } from "../types/types";
 import { MatadorProps } from "../types/matadorProps";
-import FirstSound from "./Sounds/firstSound.mp3"
-import SecondSound from "./Sounds/SecondSound.mp3"
-import ThirdSound from "./Sounds/ThirdSound.mp3"
+import FirstSound from "./Sounds/firstSound.mp3";
+import SecondSound from "./Sounds/SecondSound.mp3";
+import ThirdSound from "./Sounds/ThirdSound.mp3";
 
 export const Matador = React.memo((props: MatadorProps) => {
   const [render, isRender] = useState(false);
   const { matadorPosition, setMatarodPosition } = props;
   let { applause } = props;
-  const oldApplauseRef = useRef(applause)
+  const oldApplauseRef = useRef(applause);
 
-  if (applause === 1){
-    const firstSound = new Audio(FirstSound)
-    firstSound.play()
-  } else if (applause === 2){
-    const secondSound = new Audio(SecondSound)
-    secondSound.play()
-  } else if (applause === 3){
-    const thirdSound = new Audio(ThirdSound)
-    thirdSound.play()
-  }
+  useEffect(() => {
+    if (applause === 1) {
+      const firstSound = new Audio(FirstSound);
+      firstSound.currentTime = 0
+      firstSound.pause()
+      firstSound.play();
+    } else if (applause === 2) {
+      const secondSound = new Audio(SecondSound);
+      secondSound.currentTime = 0
+      secondSound.pause()
+      secondSound.play();
+    } else if (applause === 3) {
+      const thirdSound = new Audio(ThirdSound);
+      thirdSound.currentTime = 0
+      thirdSound.pause()
+      thirdSound.play();
+    }
+  });
 
   useEffect(() => {
     if (applause === 3) {
       let oldApplause = oldApplauseRef.current;
       if (applause !== oldApplause) {
-        oldApplauseRef.current = applause
+        oldApplauseRef.current = applause;
         isRender(true);
       } else if (applause === oldApplause) {
         isRender(false);
       }
     } else {
-      oldApplauseRef.current = applause
+      oldApplauseRef.current = applause;
     }
   }, [applause]);
   useEffect(() => {
@@ -51,9 +59,5 @@ export const Matador = React.memo((props: MatadorProps) => {
       document.removeEventListener("bullRun", bullRun as EventListener);
     };
   }, [setMatarodPosition]);
-  return (
-    <>
-      {render ? <h3>I am happy motador</h3> : <h3>i am motador</h3>}
-    </>
-  );
+  return <>{render ? <h3>I am happy motador</h3> : <h3>i am motador</h3>}</>;
 });
