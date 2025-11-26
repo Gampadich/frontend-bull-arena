47f5d727d0f3b4738c21add5929c4f9010e58c9c
Roman Diachenko <165205043+Gampadich@users.noreply.github.com>
2025-11-26 07:19:17 +0200

    create file OldMatador.tsx and made import in it

---
+++ src/components/OldMatador.tsx
@@ -0,0 +1,8 @@
+import React, { Component, useRef } from "react";
+import { BullRunEvent } from "../types/types";
+import { MatadorProps } from "../types/matadorProps";
+import FirstSound from "./Sounds/firstSound.mp3";
+import SecondSound from "./Sounds/SecondSound.mp3";
+import ThirdSound from "./Sounds/ThirdSound.mp3";
+
+
